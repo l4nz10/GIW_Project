012e22666e6c7ee97b9b5fcb95f2476dde613ba1
l4nz10 <valerio29@gmail.com>
2014-04-22 19:44:45 +0200

    Deleted useless/obsolete projects

--- GIWSearchEngine/src/azione/Azione.java
+++
@@ -1,8 +0,0 @@
-package azione;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
-public abstract class Azione {
-	public abstract String esegui(HttpServletRequest request)throws ServletException;
-}
